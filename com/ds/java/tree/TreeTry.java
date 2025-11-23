e6f67abfe6f0fe186d013c6c20f195cfad885b8c
Adithya Sv <svadithya93@gmail.com>
2025-11-24 01:39:26 +0530

    refactor

--- com/ds/java/tree/TreeTry.java
+++ com/ds/java/tree/TreeTry.java
@@ -1,22 +1,11 @@
 package com.ds.java.tree;
 
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class TreeTry {
     static Node root ;
     public static void main(String[] args) {
-        /*root = new Node(2);
-        root.left = new Node(4);
-        root.right = new Node(7);
-        root.left.left = new Node(8);
-        root.left.right = new Node(9);
-        root.left.right.left = new Node(11);
-        root.right.right = new Node(13);
-        root.right.right.left = new Node(10);*/
-
         root = insert(root, 2);
         root = insert(root, 4);
+        root = insert(root, 3);
         root = insert(root, 9);
         root = insert(root, 8);
         root = insert(root, 6);
@@ -25,8 +14,10 @@ public class TreeTry {
         root = insert(root, 11);
         root = insert(root, 10);
         root = insert(root, 13);
+        printRootToLeaftPaths(root, "");
         delete(root,4);
         printRootToLeaftPaths(root, "");
+
         System.out.println("\nPre-order traversal:");
         preOrder(root);
 
@@ -39,34 +30,34 @@ public class TreeTry {
         System.out.println("Root to Leaf paths:");
         printRootToLeaftPaths(root, "");
     }
-    public static Node delete(Node root, int data) {
-        if (root == null) {
+    public static Node delete(Node node, int data) {
+        if (node == null) {
             return null;
         }
-        if (data < root.data) {
-            root.left = delete(root.left, data);
-        } else if (data > root.data) {
-            root.right = delete(root.right, data);
+        if (data < node.data) {
+            node.left = delete(node.left, data);
+        } else if (data > node.data) {
+            node.right = delete(node.right, data);
         } else {
             // Node found
-            if (root.left == null && root.right == null) {
+            if (node.left == null && node.right == null) {
                 return null; // Case: no children
-            } else if (root.left == null) {
-                return root.right; // Case: only right child
-            } else if (root.right == null) {
-                return root.left; // Case: only left child
+            } else if (node.left == null) {
+                return node.right; // Case: only right child
+            } else if (node.right == null) {
+                return node.left; // Case: only left child
             } else {
                 // Case: two children
                 // Find inorder successor (minimum value in right subtree)
-                Node successor = root.right;
+                Node successor = node.right;
                 while (successor.left != null) {
                     successor = successor.left;
                 }
-                root.data = successor.data;
-                root.right = delete(root.right, successor.data);
+                node.data = successor.data;
+                node.right = delete(node.right, successor.data);
             }
         }
-        return root;
+        return node;
     }
 
     public static Node insert(Node root, int data) {
